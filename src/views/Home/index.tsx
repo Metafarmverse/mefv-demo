1cca8364a5a7560786926cfe0212c2a02fd7ca4d
Metafarmverse <96702109+Metafarmverse@users.noreply.github.com>
2022-03-13 15:45:49 -0800

    Update index.tsx

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -9,7 +9,6 @@ import Hero from './components/Hero'
 import { swapSectionData, earnSectionData, cakeSectionData } from './components/SalesSection/data'
 import MetricsSection from './components/MetricsSection'
 import SalesSection from './components/SalesSection'
-import WinSection from './components/WinSection'
 import FarmsPoolsRow from './components/FarmsPoolsRow'
 import CakeDataRow from './components/CakeDataRow'
 import { WedgeTopLeft, InnerWedgeWrapper, OuterWedgeWrapper, WedgeTopRight } from './components/WedgeSvgs'
@@ -118,7 +117,7 @@ const Home: React.FC = () => {
         index={2}
         hasCurvedDivider={false}
       >
-        <WinSection />
+        
       </PageSection>
       <PageSection
         innerProps={{ style: HomeSectionContainerStyles }}
