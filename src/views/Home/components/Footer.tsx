9a60f53e0255464450aeeb39f9f0d8edf20412a9
Metafarmverse <96702109+Metafarmverse@users.noreply.github.com>
2022-03-13 13:36:09 -0800

    Update Footer.tsx

--- src/views/Home/components/Footer.tsx
+++ src/views/Home/components/Footer.tsx
@@ -80,43 +80,6 @@ const bottomRightImage = {
   ],
 }
 
-const Footer = () => {
-  const { t } = useTranslation()
-  const { account } = useWeb3React()
 
-  return (
-    <>
-      <BgWrapper>
-        <Flex alignItems="center" justifyContent="center" width="100%" height="100%">
-          <StyledSunburst />
-        </Flex>
-      </BgWrapper>
-      <FloatingPancakesWrapper>
-        <TopLeftImgWrapper>
-          <CompositeImage {...topLeftImage} maxHeight="256px" />
-        </TopLeftImgWrapper>
-        <BottomRightImgWrapper>
-          <CompositeImage {...bottomRightImage} maxHeight="256px" />
-        </BottomRightImgWrapper>
-      </FloatingPancakesWrapper>
-      <Wrapper>
-        <Heading mb="24px" scale="xl" color="white">
-          {t('Start in seconds.')}
-        </Heading>
-        <Text textAlign="center" color="white">
-          {t('Connect your crypto wallet to start using the app in seconds.')}
-        </Text>
-        <Text mb="24px" bold color="white">
-          {t('No registration needed.')}
-        </Text>
-
-        <Link external href="https://docs.pancakeswap.finance/">
-          {t('Learn how to start')}
-        </Link>
-        {!account && <ConnectWalletButton mt="24px" />}
-      </Wrapper>
-    </>
-  )
-}
 
 export default Footer
