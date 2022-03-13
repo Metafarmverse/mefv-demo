6770b6740fdf96f68aa6d3f51e7da5addd745085
Metafarmverse <96702109+Metafarmverse@users.noreply.github.com>
2022-03-13 15:31:14 -0800

    Update pageSectionStyles.tsx

--- src/views/Home/pageSectionStyles.tsx
+++ src/views/Home/pageSectionStyles.tsx
@@ -1,5 +1,5 @@
-export const HERO_TOP_BG = 'linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)'
-export const HERO_BOTTOM_BG = 'linear-gradient(180deg, #FFFFFF 22%, #D7CAEC 100%)'
+export const HERO_TOP_BG = 'linear-gradient(139.73deg, #071120 0%, #071120 100%)'
+export const HERO_BOTTOM_BG = 'linear-gradient(180deg, #071120 22%, #071120 100%)'
 export const HERO_SVG_TOP =
   'linear-gradient(180deg, rgba(255, 255, 255, 0.48) 24.67%, rgba(255, 255, 255, 0.35) 67.34%, #FFFFFF 100%);'
-export const HERO_SVG_BOTTOM = '#D8CBED'
+export const HERO_SVG_BOTTOM = '#071120'
