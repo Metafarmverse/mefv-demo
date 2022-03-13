2e998bad3cbbd80227e4bb84381f3a51b60fe01d
Metafarmverse <96702109+Metafarmverse@users.noreply.github.com>
2022-03-13 06:22:35 -0800

    Update footerConfig.ts

--- src/components/Menu/config/footerConfig.ts
+++ src/components/Menu/config/footerConfig.ts
@@ -6,49 +6,19 @@ export const footerLinks: (t: ContextApi['t']) => FooterLinkType[] = (t) => [
     label: t('About'),
     items: [
       {
-        label: t('Contact'),
-        href: 'https://docs.pancakeswap.finance/contact-us',
-      },
-      {
-        label: t('Brand'),
-        href: 'https://docs.pancakeswap.finance/brand',
-      },
-      {
         label: t('Blog'),
-        href: 'https://medium.com/pancakeswap',
+        href: 'https://metafarmverse.medium.com',
       },
       {
         label: t('Community'),
-        href: 'https://docs.pancakeswap.finance/contact-us/telegram',
+        href: 'https://docs.metafarmverse.finance/contact-us/Socials',
       },
       {
-        label: t('CAKE token'),
-        href: 'https://docs.pancakeswap.finance/tokenomics/cake',
+        label: t('MEFV token'),
+        href: 'https://docs.metafarmverse.finance/whitepaper/meta-farm-verse',
       },
       {
         label: '—',
-      },
-      {
-        label: t('Online Store'),
-        href: 'https://pancakeswap.creator-spring.com/',
-        isHighlighted: true,
-      },
-    ],
-  },
-  {
-    label: t('Help'),
-    items: [
-      {
-        label: t('Customer Support'),
-        href: 'https://docs.pancakeswap.finance/contact-us/customer-support',
-      },
-      {
-        label: t('Troubleshooting'),
-        href: 'https://docs.pancakeswap.finance/help/troubleshooting',
-      },
-      {
-        label: t('Guides'),
-        href: 'https://docs.pancakeswap.finance/get-started',
       },
     ],
   },
@@ -57,23 +27,11 @@ export const footerLinks: (t: ContextApi['t']) => FooterLinkType[] = (t) => [
     items: [
       {
         label: 'Github',
-        href: 'https://github.com/pancakeswap',
+        href: 'https://github.com/Metafarmverse',
       },
       {
         label: t('Documentation'),
-        href: 'https://docs.pancakeswap.finance',
-      },
-      {
-        label: t('Bug Bounty'),
-        href: 'https://docs.pancakeswap.finance/code/bug-bounty',
-      },
-      {
-        label: t('Audits'),
-        href: 'https://docs.pancakeswap.finance/help/faq#is-pancakeswap-safe-has-pancakeswap-been-audited',
-      },
-      {
-        label: t('Careers'),
-        href: 'https://docs.pancakeswap.finance/hiring/become-a-chef',
+        href: 'https://docs.metafarmverse.finance',
       },
     ],
   },
