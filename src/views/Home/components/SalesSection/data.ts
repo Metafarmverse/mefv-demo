b5471201eb372ebd37ccfe83c304fdd2e5311234
Metafarmverse <96702109+Metafarmverse@users.noreply.github.com>
2022-03-15 14:51:34 -0800

    Update data.ts

--- src/views/Home/components/SalesSection/data.ts
+++ src/views/Home/components/SalesSection/data.ts
@@ -2,16 +2,16 @@ import { TranslateFunction } from 'contexts/Localization/types'
 import { SalesSectionProps } from '.'
 
 export const swapSectionData = (t: TranslateFunction): SalesSectionProps => ({
-  headingText: t('Trade anything. No registration, no hassle.'),
-  bodyText: t('Trade any token on BNB Smart Chain in seconds, just by connecting your wallet.'),
+  headingText: t('Enter the Metaverse, No Registration'),
+  bodyText: t('Trade, Build and Earn on BNB Smart Chain in seconds, just by connecting your wallet.'),
   reverse: false,
   primaryButton: {
-    to: '/swap',
-    text: t('Trade Now'),
+    to: '/nft',
+    text: t('Build now'),
     external: false,
   },
   secondaryButton: {
-    to: 'https://docs.pancakeswap.finance/',
+    to: 'https://docs.metafarmverse.finance/',
     text: t('Learn'),
     external: true,
   },
@@ -25,43 +25,21 @@ export const swapSectionData = (t: TranslateFunction): SalesSectionProps => ({
   },
 })
 
-export const earnSectionData = (t: TranslateFunction): SalesSectionProps => ({
-  headingText: t('Earn passive income with crypto.'),
-  bodyText: t('PancakeSwap makes it easy to make your crypto work for you.'),
-  reverse: true,
-  primaryButton: {
-    to: '/farms',
-    text: t('Explore'),
-    external: false,
-  },
-  secondaryButton: {
-    to: 'https://docs.pancakeswap.finance/products/yield-farming',
-    text: t('Learn'),
-    external: true,
-  },
-  images: {
-    path: '/images/home/earn/',
-    attributes: [
-      { src: 'pie', alt: t('Pie chart') },
-      { src: 'stonks', alt: t('Stocks chart') },
-      { src: 'folder', alt: t('Folder with cake token') },
-    ],
-  },
-})
+
 
 export const cakeSectionData = (t: TranslateFunction): SalesSectionProps => ({
-  headingText: t('CAKE makes our world go round.'),
+  headingText: t('MEFV Governs the Metaverse.'),
   bodyText: t(
-    'CAKE token is at the heart of the PancakeSwap ecosystem. Buy it, win it, farm it, spend it, stake it... heck, you can even vote with it!',
+    'MEFV Token is the Governance token of Meta Farm Verse. Buy it, win it, farm it, spend it, stake it... heck, you can even vote with it!',
   ),
   reverse: false,
   primaryButton: {
-    to: '/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-    text: t('Buy CAKE'),
+    to: 'https://app.metafarmverse.finance',
+    text: t('Buy MEFV'),
     external: false,
   },
   secondaryButton: {
-    to: 'https://docs.pancakeswap.finance/tokenomics/cake',
+    to: 'https://docs.metafarmverse.finance',
     text: t('Learn'),
     external: true,
   },
