4bf86d9896d0c66463889f5ee8a1f0de59575b05
Metafarmverse <96702109+Metafarmverse@users.noreply.github.com>
2022-03-16 09:03:31 -0800

    Update Global.tsx

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -8,7 +8,7 @@ declare module 'styled-components' {
 
 const GlobalStyle = createGlobalStyle`
   * {
-    font-family: 'Open Sans', sans-serif;
+    font-family: 'Kanit', sans-serif;
   }
   body {
     background-color: ${({ theme }) => theme.colors.background};
